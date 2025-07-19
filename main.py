3b86a75c0735311d5be5d089f776862d3132451a
talhakhan7640 <talhakhan7640@gmail.com>
2025-07-19 19:51:38 +0530

    resolved oauth issue

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ oauth.register(
     client_secret=os.getenv('GOOGLE_CLIENT_SECRET'),
     server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
     client_kwargs={'scope': 'openid email profile'},
+    redirect_uri='http://localhost:8000/auth/google/callback'
 )
 
 # FastAPI App
